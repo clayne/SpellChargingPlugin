76a243c111ee10f1851492ed0dee1d22ffca9872
vitali.stabel <vitali.stabel@outlook.de>
2021-08-26 17:09:04 +0200

    update

--- Core/ChargingSpell.cs
+++ Core/ChargingSpell.cs
@@ -15,21 +15,24 @@ namespace SpellChargingPlugin.Core
         public EquippedSpellSlots Slot { get; set; }
         public State<ChargingSpell> CurrentState { get; set; }
 
-        private Dictionary<ActiveEffect.EffectItem, SpellPower> BasePower { get; set; }
-        private Dictionary<ActiveEffect.EffectItem, PowerModifier>  Modifier { get; set; }
-        private int ChargeLevel { get; set; } = 0;
+        private Dictionary<ActiveEffect.EffectItem, SpellPower> _basePower;
+        private Dictionary<ActiveEffect.EffectItem, PowerModifier> _powerModifier;
+        private int _chargeLevel;
 
         public ChargingSpell(ChargingActor holder, SpellItem spell, EquippedSpellSlots slot)
         {
             Holder = holder;
             Spell = spell;
             Slot = slot;
-            BasePower = new Dictionary<ActiveEffect.EffectItem, SpellPower>();
-            Modifier = new Dictionary<ActiveEffect.EffectItem, PowerModifier>();
+
+            _basePower = new Dictionary<ActiveEffect.EffectItem, SpellPower>();
+            _powerModifier = new Dictionary<ActiveEffect.EffectItem, PowerModifier>();
+            _chargeLevel = 0;
+
             foreach (var eff in Spell.Effects)
             {
-                BasePower.Add(eff, new SpellPower(eff.Magnitude, eff.Duration));
-                Modifier.Add(eff, new PowerModifier(0f, 0));
+                _basePower.Add(eff, new SpellPower(eff.Magnitude, eff.Duration));
+                _powerModifier.Add(eff, new PowerModifier(0f, 0));
             }
 
             var factory = new StateFactory<ChargingSpell>();
@@ -41,38 +44,53 @@ namespace SpellChargingPlugin.Core
         {
             foreach (var eff in Spell.Effects)
             {
-                eff.Magnitude = BasePower[eff].Magnitude + Modifier[eff].Magnitude;
-                eff.Duration = BasePower[eff].Duration + Modifier[eff].Duration;
+                eff.Magnitude = _basePower[eff].Magnitude + _powerModifier[eff].Magnitude;
+                eff.Duration = _basePower[eff].Duration + _powerModifier[eff].Duration;
             }
         }
 
         public void Charge(float percentage)
         {
+            var magCost = (int)(Spell.SpellData.CostOverride * percentage);
+            if (!HasMagickaForCharge(magCost))
+                return;
+            DrainMagicka(magCost);
+
             foreach (var eff in Spell.Effects)
             {
                 bool hasMag = eff.Magnitude > 0f;
                 bool hasDur = eff.Duration > 0;
                 float realPercantage = percentage * (hasMag && hasDur ? 0.5f : 1f);
 
-                Modifier[eff].Magnitude += BasePower[eff].Magnitude * realPercantage;
-                Modifier[eff].Duration += (int)(BasePower[eff].Duration * realPercantage);
+                _powerModifier[eff].Magnitude += _basePower[eff].Magnitude * realPercantage;
+                _powerModifier[eff].Duration += (int)(_basePower[eff].Duration * realPercantage);
 
-                ++ChargeLevel;
-
-                DebugHelper.Print($"Charge {Spell.Name}.{eff.Effect.Name} bMAG: {Modifier[eff].Magnitude}, bDur: {Modifier[eff].Duration}");
+                DebugHelper.Print($"Charge {Spell.Name}.{eff.Effect.Name} bMAG: {_powerModifier[eff].Magnitude}, bDur: {_powerModifier[eff].Duration}");
             }
+            ++_chargeLevel;
+            UpdateVisual();
+        }
+
+        private void DrainMagicka(int magCost)
+        {
+            Holder.Character.DamageActorValue(ActorValueIndices.Magicka, magCost);
+        }
+
+        private bool HasMagickaForCharge(int magCost)
+        {
+            return Holder.Character.GetActorValue(ActorValueIndices.Magicka) >= magCost;
         }
 
         public void Reset()
         {
             foreach (var eff in Spell.Effects)
             {
-                Modifier[eff].Magnitude = BasePower[eff].Magnitude;
-                Modifier[eff].Duration+= BasePower[eff].Duration;
+                _powerModifier[eff].Magnitude = _basePower[eff].Magnitude;
+                _powerModifier[eff].Duration+= _basePower[eff].Duration;
 
                 DebugHelper.Print($"Reset {Spell.Name}");
             }
-            ChargeLevel = 0;
+            _chargeLevel = 0;
         }
 
         public void UpdateVisual()
